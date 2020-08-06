1e4a8b31a0a001fc40c8018f351e209c2783ede8
Brandon Cooper <bc3700@rit.edu>
2020-08-06 16:43:40 -0400

    Updates

---
+++ precache-manifest.f001c7301ae1baac4df9f46ef0d31e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e598894d37446cff0f2026f84f7f16",
+    "url": "/tutorial-todo-app/index.html"
+  },
+  {
+    "revision": "1f0836e22417e1ff9365",
+    "url": "/tutorial-todo-app/static/css/main.502cdf70.chunk.css"
+  },
+  {
+    "revision": "9fe7206958d2fd3d87f0",
+    "url": "/tutorial-todo-app/static/js/2.c468dc1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-todo-app/static/js/2.c468dc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0836e22417e1ff9365",
+    "url": "/tutorial-todo-app/static/js/main.7b1370e4.chunk.js"
+  },
+  {
+    "revision": "dec4e0b75f0aef86732c",
+    "url": "/tutorial-todo-app/static/js/runtime-main.0d4117b1.js"
+  }
+]);
